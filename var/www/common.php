9de11a972299a0af37ab0c39e681c38fbeaf2923
Daniel Winzen <d@winzen4.de>
2018-11-24 10:38:59 +0100

    Dropped PHP7.1 support and install composer

--- var/www/common.php
+++ var/www/common.php
@@ -20,7 +20,7 @@ const REQUIRE_APPROVAL=false; //require admin approval of new sites? true/false
 const ADMIN_PASSWORD='MY_PASSWORD'; //password for admin interface
 const SERVICE_INSTANCES=['2', '3', '4', '5', '6', '7', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const DISABLED_PHP_VERSIONS=[];
-const PHP_VERSIONS=[2 => '7.1', 3 => '7.2', 4 => '7.3'];
+const PHP_VERSIONS=[3 => '7.2', 4 => '7.3'];
 const PHP_CONFIG='memory_limit = 256M
 error_reporting = E_ALL
 post_max_size = 10G
